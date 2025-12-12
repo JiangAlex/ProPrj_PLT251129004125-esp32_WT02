36414f782031504c6b4739122bbfe5a89f673fc9
JiangAlex <jiang0828@gmail.com>
2025-12-12 21:19:43 +0800

    create a lib

---
+++ lib/MicroAPRS/protocol/HDLC.h
@@ -0,0 +1,14 @@
+// Copyright Mark Qvist / unsigned.io
+// https://unsigned.io/microaprs
+//
+// Licensed under GPL-3.0. For full info,
+// read the LICENSE file.
+
+#ifndef PROTOCOL_HDLC_H
+#define PROTOCOL_HDLC_H
+
+#define HDLC_FLAG  0x7E
+#define HDLC_RESET 0x7F
+#define AX25_ESC   0x1B
+
+#endif
